9f55fca531b9ca96b94cd54ade23a74b11f4c6e8
sunat <sunat@DESKTOP-LD2DID4.localdomain>
2024-11-29 17:16:17 +0500

    2

--- api/user_handler.go
+++ api/user_handler.go
@@ -1,18 +1,34 @@
 package api
 
 import (
+	"context"
+
 	"github.com/gofiber/fiber/v2"
-	"github.com/ssssunat/hotel-reservation/types"
+	"github.com/ssssunat/hotel-reservation/db"
 )
 
-func HandleGetUsers(c *fiber.Ctx) error {
-	u := types.User{
-		FirstName: "James",
-		LastName: "At the watercooler",
-	}
-	return c.JSON(u)
+type UserHandler struct {
+	userStore db.UserStore
 }
 
-func HandleGetUser(c *fiber.Ctx) error {
+func NewUserHandler(userStore db.UserStore) *UserHandler {
+	return &UserHandler{
+		userStore: userStore,
+	}
+}
+
+func (h *UserHandler) HandleGetUsers(c *fiber.Ctx) error {
+	var (
+		id = c.Params("id")
+		ctx = context.Background()
+	)
+	user, err := h.userStore.GetUserByID(ctx, id)
+	if err != nil {
+		return err
+	}
+	return c.JSON(user)
+}
+
+func (h *UserHandler) HandleGetUser(c *fiber.Ctx) error {
 	return c.JSON("James")
 }
--- main.go
+++ main.go
@@ -1,21 +1,43 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"log"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/ssssunat/hotel-reservation/api"
+	"github.com/ssssunat/hotel-reservation/db"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+const dburi = "mongodb://localhost:27017"
+const dbname = "hotel-reservation"
+const userColl = "users"
+
+var config = fiber.Config{
+	ErrorHandler: func(c *fiber.Ctx, err error) error {
+		return c.JSON(map[string]string{"error": err.Error()})
+	},
+}
 
 func main() {
 	listenAddr := flag.String("listenAddr", ":5000", "The listen addr of the API server")
 	flag.Parse()
 
-	app := fiber.New()
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(dburi))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// handler init
+	userHandler := api.NewUserHandler(db.NewMongoUserStore(client))
+	app := fiber.New(config)
 	apiv1 := app.Group("api/v1")
 
-	apiv1.Get("/user", api.HandleGetUsers)
-	apiv1.Get("/user/:id", api.HandleGetUser)
+	apiv1.Get("/user", userHandler.HandleGetUser)
+	apiv1.Get("/user/:id", userHandler.HandleGetUsers)
 
 	app.Listen(*listenAddr)
 }
@@ -23,4 +45,3 @@ func main() {
 func handleFoo(c *fiber.Ctx) error {
 	return c.JSON(map[string]string{"msg": "working just fine!"})
 }
-
