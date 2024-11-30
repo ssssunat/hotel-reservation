ff1eafe5ab2b675de395fc76a1ce06906437b800
sunat <sunat@DESKTOP-LD2DID4.localdomain>
2024-12-01 01:07:04 +0500

    3

--- api/user_handler.go
+++ api/user_handler.go
@@ -1,10 +1,9 @@
 package api
 
 import (
-	"context"
-
 	"github.com/gofiber/fiber/v2"
 	"github.com/ssssunat/hotel-reservation/db"
+	"github.com/ssssunat/hotel-reservation/types"
 )
 
 type UserHandler struct {
@@ -17,12 +16,30 @@ func NewUserHandler(userStore db.UserStore) *UserHandler {
 	}
 }
 
+func (h *UserHandler) HandlePostUser(c *fiber.Ctx) error {
+	var params types.CreateUserParams
+	if err := c.BodyParser(&params); err != nil {
+		return err
+	}
+	if errors := params.Validate(); len(errors) >= 0 {
+		return c.JSON(errors)
+	}
+	user, err := types.NewUserFromParams(params)
+	if err != nil {
+		return err
+	}
+	insertedUser, err := h.userStore.InsertUser(c.Context(), user)
+	if err != nil {
+		return err
+	}
+	return c.JSON(insertedUser)
+}
+
 func (h *UserHandler) HandleGetUsers(c *fiber.Ctx) error {
 	var (
 		id = c.Params("id")
-		ctx = context.Background()
 	)
-	user, err := h.userStore.GetUserByID(ctx, id)
+	user, err := h.userStore.GetUserByID(c.Context(), id)
 	if err != nil {
 		return err
 	}
@@ -30,5 +47,9 @@ func (h *UserHandler) HandleGetUsers(c *fiber.Ctx) error {
 }
 
 func (h *UserHandler) HandleGetUser(c *fiber.Ctx) error {
-	return c.JSON("James")
+	users, err := h.userStore.GetUsers(c.Context())
+	if err != nil {
+		return err
+	}
+	return c.JSON(users)
 }
