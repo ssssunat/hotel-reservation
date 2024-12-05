fce78e9af824ec03a14185cde6ff9c4e0befa8a8
sunat <sunat@DESKTOP-LD2DID4.localdomain>
2024-12-06 04:18:32 +0500

    Hotel API

---
+++ api/hotel_handler.go
@@ -0,0 +1,55 @@
+package api
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/ssssunat/hotel-reservation/db"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type HotelHandler struct {
+	store *db.Store
+	roomStore  db.RoomStore
+	hotelStore db.HotelStore
+}
+
+func NewHotelHandler(store *db.Store) *HotelHandler {
+	return &HotelHandler{
+		store: store,
+	}
+}
+
+func (h *HotelHandler) HandleGetRooms(c *fiber.Ctx) error {
+	id := c.Params("id")
+	oid, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return err
+	} 
+	filter := bson.M{"hotelID": oid}
+	rooms, err := h.store.Room.GetRooms(c.Context(), filter)
+	if err != nil {
+		return err
+	}
+	return c.JSON(rooms)
+}
+
+func (h *HotelHandler) HandleGetHotels(c *fiber.Ctx) error {
+	hotels, err := h.store.Hotel.GetHotels(c.Context(), nil)
+	if err != nil {
+		return err
+	}
+	return c.JSON(hotels)
+}
+
+func (h *HotelHandler) HandleGetHotel(c *fiber.Ctx) error {
+	id := c.Params("id")
+	oid, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return err
+	} 
+	hotel, err := h.store.Hotel.GetHotelByID(c.Context(), oid)
+	if err != nil {
+		return err
+	}
+	return c.JSON(hotel)
+}
--- types/hotel.go
+++ types/hotel.go
@@ -7,22 +7,13 @@ type Hotel struct {
 	Name     string               `bson:"name" json:"name"`
 	Location string               `bson:"location" json:"location"`
 	Rooms    []primitive.ObjectID `bson:"rooms" json:"rooms"`
+	Rating   int                  `bson:"rating" json:"rating"`
 }
-
-type RoomType int
-
-const (
-	None RoomType = iota
-	SinglePersonRoomType
-	DoublePersonRoomType
-	SeaSideRoomType
-	DeluxeRoomType
-)
 
 type Room struct {
 	ID        primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
-	Type      RoomType           `bson:"type" json:"type"`
-	BasePrice float64            `bson:"basePrice" json:"basePrice"`
+	Size		string				`bson:"size" json:"size"`
+	Seaside   bool 				 `bson:"seaside json:"seaside"`	
 	Price     float64            `bson:"price" json:"price"`
 	HotelID   primitive.ObjectID `bson:"hotelID" json:"hotelID"`
 }
